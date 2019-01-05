8319709f929ddc9be0242069a1d147019e17a990
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-05 07:53:09 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
 
   def create
     # raise params.inspect
-    @school_class = SchoolClass.create post_params(:first_name, :last_name)
+    @school_class = SchoolClass.create post_params(:title, :room_number)
     redirect_to school_class_path(@school_class)
   end
 
